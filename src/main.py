7b286dec5619b768c2bf1ac078048d8f9965886b
gallo-json <gallojsantiago@gmail.com>
2020-08-07 09:29:53 -0500

    everything works thus far

--- src/main.py
+++ src/main.py
@@ -1,7 +1,3 @@
-# ssh pi@camera.local
-# pass: badgersandfoxes
-
-# /NaturewatchCameraServer/www/photos
 from tensorflow.keras.models import load_model
 from paramiko import SSHClient
 from scp import SCPClient
@@ -9,32 +5,30 @@ from matplotlib import pyplot as plt
 from matplotlib.image import imread
 from socket import gaierror
 import numpy as np
-import os
 import cv2
+import sys
+import os
 
 temp = '/tmp/photos'
 
-os.system('mkdir ' + temp)
+def progress(filename, size, sent):
+    sys.stdout.write("%s's progress: %.2f%%   \r" % (filename, float(sent)/float(size)*100) )
 
 try:
+
     ssh = SSHClient()
     ssh.load_system_host_keys()
     ssh.connect(hostname='camera.local', 
-                #port = 'port',
                 username='pi',
                 password='badgersandfoxes')
 
-
-    # SCPCLient takes a paramiko transport as its only argument
-    scp = SCPClient(ssh.get_transport())
+    scp = SCPClient(ssh.get_transport(), progress=progress)
 
     scp.get('~/NaturewatchCameraServer/www/photos', temp, recursive=True)
 
     assert len(os.listdir(temp)) != 0
 
-    scp.close()
-
-    stdin, stdout, stderr = ssh.exec_command('rm -rf ~/NaturwatchCameraServer/www/photos/*.jpg')
+    stdin, stdout, stderr = ssh.exec_command('rm -rf ~/NaturwatchCameraServer/www/photos/*')
 
     for line in stdout.readlines(): print(line)
 
@@ -49,6 +43,8 @@ for raw in os.listdir(temp):
         image = cv2.resize(imread(temp + raw), (120, 68))
         if np.all(image.shape == (68, 120, 3)):
             inputs.append(np.array(image))
+    else:
+        os.system('rm ' + raw)
 
 #model = load_model('model/MyNaturewatchCNN')
 
